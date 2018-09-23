cfea3b90e5a7af1ce595deb8d8add48c2ebb11e7
chin-wag <svetlanafedorishcheva99@gmail.com>
2018-09-23 20:52:51 +0500

    создан метод для чтения и обработки вопросов викторины

--- src/Game.java
+++ src/Game.java
@@ -1,11 +1,26 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 class Game {
-    private Integer score = 0;
-    private List<QuestionAnswerPair> data = new ArrayList<>();
-    private QuestionAnswerPair curPair;
-    private Integer curPairIndex = 0;
-    Boolean isGameContinued = true;
+  private Integer score = 0;
+  private List<QuestionAnswerPair> data = new ArrayList<>();
+  private QuestionAnswerPair curPair;
+  private Integer curPairIndex = 0;
+  Boolean isGameContinued = true;
 
+
+  private void handleData() {
+    try {
+      var reader = new BufferedReader(new FileReader("src/data"));
+      String text;
+
+      while ((text = reader.readLine()) != null)
+        data.add(new QuestionAnswerPair(text.split(" ")));
+    } catch (IOException ex) {
+        System.out.println(ex.getMessage());
+    }
+  }
 }
