677a2d4da27c37a0d0183e36dfd07028c07fe0a8
chin-wag <svetlanafedorishcheva99@gmail.com>
2018-09-23 20:42:31 +0500

    добавлены поля класса Game

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,11 @@
-public class Game {
+import java.util.ArrayList;
+import java.util.List;
+
+class Game {
+    private Integer score = 0;
+    private List<QuestionAnswerPair> data = new ArrayList<>();
+    private QuestionAnswerPair curPair;
+    private Integer curPairIndex = 0;
+    Boolean isGameContinued = true;
+
 }
