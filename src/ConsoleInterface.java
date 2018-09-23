916359cf9dfd1ac906bcc17d18b8b439b24a36f8
Ratbek Nurlanov <ratbeknurlanov@gmail.com>
2018-09-23 20:55:25 +0500

    Написана основная структура ( методы и поля ) класса

--- src/ConsoleInterface.java
+++ src/ConsoleInterface.java
@@ -1,2 +1,26 @@
+import java.io.PrintStream;
+import java.util.Scanner;
+
 public class ConsoleInterface {
+  private static Scanner in = new Scanner(System.in);
+  private static PrintStream out = new PrintStream(System.out);
+  private static Game game = new Game();
+
+  public static void main(String[] args) {
+    salute();
+    play();
+  }
+
+  private static void play() {
+  }
+
+  private static void doStep() {
+  }
+
+  private static void salute() {
+  }
+
+  private static Boolean isUserInputCommand(String input) {
+    return null;
+  }
 }
