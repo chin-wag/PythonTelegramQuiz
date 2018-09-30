f249a13f416adf6008a4fbaaf3055dfec60ea46c
chinwag <svetlanafedorishcheva99@gmail.com>
2018-10-01 00:27:23 +0500

    доюавлен тест на метод doStep

--- src/ConsoleInterfaceTest.java
+++ src/ConsoleInterfaceTest.java
@@ -1,4 +1,9 @@
+import javafx.util.Pair;
 import org.junit.jupiter.api.Test;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,6 +21,42 @@ class ConsoleInterfaceTest {
       assertFalse((Boolean) isUserInputCommandMethod.invoke(consoleInterface, "smthg"));
     } catch (Exception e) {
       System.out.println(e.getMessage());
+    }
+  }
+
+  @Test
+  void testDoStep() {
+    var stdin = System.in;
+    var stdout = System.out;
+    try {
+      var data = new ArrayList<Pair<String, String>>() {{
+        add(new Pair<>("4", "true"));
+        add(new Pair<>("4", "false"));
+        add(new Pair<>("/score", "1"));
+      }};
+
+      for (var pair : data) {
+        var outputContent = new ByteArrayOutputStream();
+        System.setIn(new ByteArrayInputStream(pair.getKey().getBytes()));
+        System.setOut(new PrintStream(outputContent));
+        var consoleInterface = new ConsoleInterface();
+
+        var gameField = consoleInterface.getClass().getDeclaredField("game");
+        gameField.setAccessible(true);
+        gameField.set(consoleInterface, new Game());
+
+        var doStepMethod = consoleInterface.getClass().getDeclaredMethod("doStep");
+        doStepMethod.setAccessible(true);
+        doStepMethod.invoke(consoleInterface);
+
+        var answer = outputContent.toString().split("[ \r\n]");
+        assertEquals(pair.getValue(), answer[answer.length - 1]);
+      }
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    } finally {
+      System.setIn(stdin);
+      System.setOut(stdout);
     }
   }
 }
