0a16dcdd00283b726d6dbd0a9fc33cf766e9c009
chinwag <svetlanafedorishcheva99@gmail.com>
2018-09-30 21:04:59 +0500

    добавлен тест на метод isUserInputCommand

--- src/ConsoleInterfaceTest.java
+++ src/ConsoleInterfaceTest.java
@@ -1,2 +1,20 @@
-public class ConsoleInterfaceTest {
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConsoleInterfaceTest {
+
+  @Test
+  void testIsUserInputCommand() {
+    var consoleInterface = new ConsoleInterface();
+    try {
+      var isUserInputCommandMethod = consoleInterface.getClass().getDeclaredMethod("isUserInputCommand", String.class);
+      isUserInputCommandMethod.setAccessible(true);
+
+      assertTrue((Boolean) isUserInputCommandMethod.invoke(consoleInterface, "/stop"));
+      assertTrue((Boolean) isUserInputCommandMethod.invoke(consoleInterface, "/smthg"));
+      assertFalse((Boolean) isUserInputCommandMethod.invoke(consoleInterface, "smthg"));
+    } catch (Exception e) {
+      System.out.println(e);
+    }
+  }
 }
