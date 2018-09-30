545b1023b0b6c4f80945eb2459fd0b7f5f5bd512
chinwag <svetlanafedorishcheva99@gmail.com>
2018-09-30 20:20:43 +0500

    добавлен тест на метод checkAnswer

--- src/GameTest.java
+++ src/GameTest.java
@@ -3,8 +3,21 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class GameTest {
   @Test
-  void testGetScore() throws DataHandlingException{
+  void testGetScore() throws DataHandlingException {
     var game = new Game();
-    assertEquals("0", game.getScore());
+    assertEquals(0, game.getScore());
+  }
+
+  @Test
+  void testCheckAnswer() throws DataHandlingException {
+    var game = new Game();
+    try {
+      var curPairField = game.getClass().getDeclaredField("curPair");
+      curPairField.setAccessible(true);
+      var cur = curPairField.get(game).getClass().getDeclaredField("answer");
+      assertTrue(game.checkAnswer(cur.get(curPairField.get(game)).toString()));
+    } catch (Exception e) {
+      System.out.println(e);
+    }
   }
 }
