30dfd006ed077a40a87664ebfc53e4d3edcbcb16
chinwag <svetlanafedorishcheva99@gmail.com>
2018-10-21 20:23:26 +0500

    создан класс GameManager, который обработывает поступающие запросы, хранит все созданные на данный момент игры

--- src/GameManager.java
+++ src/GameManager.java
@@ -1,40 +1,50 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class GameManager {
-  private Map<Integer, Game> games = new HashMap<>() {};
-  private QuestionManager questionManager;
+class GameManager {
+  private Map<Long, Game> games = new HashMap<>() {};
 
-  GameManager() {
+  private void addNewUser(Long id) {
     try {
-      questionManager = new QuestionManager();
+      games.put(id, new Game(new QuestionManager()));
     } catch (DataHandlingException e) {
       System.out.println(e.getMessage());
 //      TODO: обработать выход в этом случае
     }
   }
 
-  private void addNewUser(Integer id) {
-    games.put(id, new Game(questionManager));
+  private Boolean isNewUser(Long id) {
+    return !games.containsKey(id);
   }
 
-  private Boolean isNewUser(Integer id) {
-    return games.containsKey(id);
+  String getQuestion(Long id) {
+    return games.get(id).getCurrentQuestion();
   }
 
-  String handleUserRequest(Integer id, String input) {
+  private String salute() {
+    return "Это математическая викторина. Я задаю вопрос - вы отвечаете.\n" +
+            "За каждый правильный ответ получаете очки. Начинаем!";
+  }
+
+  String handleUserRequest(Long id, String input) {
     if (isNewUser(id)) {
       addNewUser(id);
+      return salute() + "\n" + UserCommand.HELP.execute(games.get(id));
+    }
+
+    if (!isGameContinued(id)) {
+      return "Викторина окончена. Количество очков - " + getScore(id);
     }
 
     if (UserCommand.isUserInputCommand(input)) {
-      return handleUserCommand(id, input);
+      return handleUserCommand(id, input) + (isGameContinued(id)
+              ? "" : "Викторина окончена. Количество очков - " + getScore(id));
     } else {
       return Boolean.toString(games.get(id).checkAnswer(input));
     }
   }
 
-  private String handleUserCommand(Integer id, String userInput) {
+  private String handleUserCommand(Long id, String userInput) {
     var userCommand = userInput.substring(1).toUpperCase();
     if (UserCommand.isValidUserCommand(userCommand)){
       var command = UserCommand.valueOf(userCommand);
@@ -42,5 +52,13 @@ public class GameManager {
     } else {
       return "Команды " + userInput + " не существует";
     }
+  }
+
+  Boolean isGameContinued(Long id) {
+    return games.get(id).isGameContinued;
+  }
+
+  private Integer getScore(Long id) {
+    return games.get(id).getScore();
   }
 }
