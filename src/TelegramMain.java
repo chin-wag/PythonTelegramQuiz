0e528b4ed0f7d944a6818d1f347575b514ef4751
chinwag <svetlanafedorishcheva99@gmail.com>
2018-10-21 20:25:26 +0500

    создан класс для звапуска при работе с телеграмом, использует telegrambots

--- src/TelegramMain.java
+++ src/TelegramMain.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
 
 public class TelegramMain {
+  public static void main(String[] args) {
+    ApiContextInitializer.init();
+    var telegramBotsApi = new TelegramBotsApi();
+    var telegramInterface = new TelegramInterface();
+    try {
+      telegramBotsApi.registerBot(telegramInterface);
+    } catch (TelegramApiException e) {
+      e.printStackTrace();
+    }
+  }
 }
