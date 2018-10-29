fa8ab788fb5ed5231402456733bb9d09e513e08e
chinwag <svetlanafedorishcheva99@gmail.com>
2018-10-29 09:21:02 +0500

    добавлено чтение данных из бд

--- src/QuestionManager.java
+++ src/QuestionManager.java
@@ -1,5 +1,4 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ListIterator;
@@ -13,6 +12,14 @@ class QuestionManager implements QuestionManagerInterface {
   private List<QuestionAnswerPair> data = new ArrayList<>();
   private ListIterator<QuestionAnswerPair> questionIterator;
 
+  private static final String url = "jdbc:mysql://localhost:3306/quiz";
+  private static final String user = "root";
+  private static final String password = "12345";
+
+  private Connection connection;
+  private Statement statement;
+  private ResultSet resultSet;
+
   QuestionManager() throws DataHandlingException{
     handleData();
     questionIterator = data.listIterator();
@@ -24,14 +31,21 @@ class QuestionManager implements QuestionManagerInterface {
 
   private void handleData() throws DataHandlingException{
     try{
-      var reader = new BufferedReader(new FileReader("data"));
-      String text;
+      connection = DriverManager.getConnection(url, user, password);
+      statement = connection.createStatement();
+      resultSet = statement.executeQuery("select question, answer from questions_and_answers");
 
-      while ((text = reader.readLine()) != null){
-        data.add(new QuestionAnswerPair(text));
+      while (resultSet.next()) {
+        var question = resultSet.getString(1);
+        var answer = resultSet.getString(2);
+        data.add(new QuestionAnswerPair(question, answer));
       }
     } catch (Exception e){
       throw new DataHandlingException(e);
+    } finally {
+      try { if (connection != null) connection.close(); } catch(SQLException se) { /*can't do anything */ }
+      try { if (statement != null) statement.close(); } catch(SQLException se) { /*can't do anything */ }
+      try { if (resultSet != null) resultSet.close(); } catch(SQLException se) { /*can't do anything */ }
     }
   }
 }
