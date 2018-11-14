c7afdac63fffdf363b7c04d48042fefbf2d3c415
chinwag <svetlanafedorishcheva99@gmail.com>
2018-11-14 19:14:41 +0500

    работа с бд вынесена в отдельный класс

--- src/QuestionManager.java
+++ src/QuestionManager.java
@@ -1,49 +1,25 @@
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.Serializable;
 import java.util.ListIterator;
 import java.util.Optional;
 
-interface QuestionManagerInterface {
+interface QuestionManagerInterface extends Serializable {
   Optional<QuestionAnswerPair> getNextPair();
 }
 
 class QuestionManager implements QuestionManagerInterface {
-  private List<QuestionAnswerPair> data = new ArrayList<>();
   private ListIterator<QuestionAnswerPair> questionIterator;
 
-  QuestionManager() throws DataHandlingException{
-    handleData();
+  QuestionManager(Long id) throws DataHandlingException{
+    var data = DataBaseManager.getData(id);
     questionIterator = data.listIterator();
+  }
+
+  QuestionManager() throws DataHandlingException {
+    this((long)1);
   }
 
   public Optional<QuestionAnswerPair> getNextPair() {
     return  questionIterator.hasNext() ? Optional.of(questionIterator.next()) : Optional.empty();
-  }
-
-  private void handleData() throws DataHandlingException{
-    EntityManagerFactory emf = null;
-    EntityManager em = null;
-    try {
-      emf = Persistence.createEntityManagerFactory("QuizUnit");
-      em = emf.createEntityManager();
-      var result = em.createQuery("select p from QuestionAnswerPair p").getResultList();
-      for(var elem : result){
-        data.add((QuestionAnswerPair) elem);
-      }
-    } catch (Exception e) {
-      throw new DataHandlingException(e);
-    }
-    finally {
-      try {
-      if (emf != null)
-        emf.close();
-      if (em != null)
-        em.close();
-      } catch (IllegalStateException e) {/*can't do anything*/}
-    }
   }
 }
 
