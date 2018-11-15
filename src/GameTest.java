0b9e62a5523875e805a3a1eab27533dda7e74b01
chinwag <svetlanafedorishcheva99@gmail.com>
2018-11-15 22:36:19 +0500

    исправлен формат тестов

--- src/GameTest.java
+++ src/GameTest.java
@@ -4,19 +4,22 @@ import static org.junit.jupiter.api.Assertions.*;
 class GameTest {
   @Test
   void testGetScore() {
-    var game = new Game(new QuestionManagerMock(new QuestionAnswerPair("2**11", "2048")));
+    var game = new Game(new QuestionManagerMock(new QuestionAnswerPair("2**11", "2048")),
+            new DatabaseManagerMock());
     assertEquals(0, game.getScore());
   }
 
   @Test
   void testCheckAnswer() {
-      var game = new Game(new QuestionManagerMock(new QuestionAnswerPair("2+2*2", "6")));
+      var game = new Game(new QuestionManagerMock(new QuestionAnswerPair("2+2*2", "6")),
+              new DatabaseManagerMock());
       assertTrue(game.checkAnswer("6"));
   }
 
   @Test
   void testStopGame() {
-    var game = new Game(new QuestionManagerMock(new QuestionAnswerPair("5*5*5", "125")));
+    var game = new Game(new QuestionManagerMock(new QuestionAnswerPair("5*5*5", "125")),
+            new DatabaseManagerMock());
     assertTrue(game.isGameContinued);
 
     game.stopGame();
@@ -25,7 +28,8 @@ class GameTest {
 
   @Test
   void testGetCurrentQuestion() {
-    var game = new Game(new QuestionManagerMock(new QuestionAnswerPair("10-1","9")));
+    var game = new Game(new QuestionManagerMock(new QuestionAnswerPair("10-1","9")),
+            new DatabaseManagerMock());
     assertEquals("10-1", game.getCurrentQuestion());
   }
 }
