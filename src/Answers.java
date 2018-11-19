dc6e3bb2510778196e95a62c8331d9ded32c9202
chinwag <svetlanafedorishcheva99@gmail.com>
2018-11-19 15:41:15 +0500

    добавлено описание для error

--- src/Answers.java
+++ src/Answers.java
@@ -8,6 +8,9 @@ enum Answers {
     String getMessage() {
       return "Неправильный ответ";
     }
+  },
+  ERROR {
+    String getMessage() { return "На сервере произошла ошибка.\nПопробуйте зайти позже"; }
   };
 
   abstract String getMessage();
