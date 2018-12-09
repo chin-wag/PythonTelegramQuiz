80576fcfd76db3512da2939b2ff1944447470dbf
ratbek <ratbeknurlanov@gmail.com>
2018-12-09 15:00:21 +0500

    Create several Game and QuestionAnswerPair database managers

---
+++ src/main/java/GameDatabaseManager.java
@@ -0,0 +1,50 @@
+package main.java;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.Optional;
+
+public class GameDatabaseManager {
+  private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("QuizUnit");
+  private EntityManager em = emf.createEntityManager();
+
+  public void save(Game game) {
+    var tx = em.getTransaction();
+    tx.begin();
+    em.persist(game);
+    tx.commit();
+  }
+
+  public void update(Game game)
+  {
+    var tx = em.getTransaction();
+    tx.begin();
+    em.merge(game);
+    tx.commit();
+  }
+
+  public void remove(long id) throws DataHandlingException
+  {
+    var existentGame = getExistent(id);
+    var tx = em.getTransaction();
+    tx.begin();
+
+    if (!em.contains(existentGame)) {
+      existentGame = em.merge(existentGame);
+    }
+    em.remove(existentGame);
+    tx.commit();
+  }
+
+  public Optional<Game> get(long id)
+  {
+    return Optional.ofNullable(em.find(Game.class, id));
+  }
+
+  public Game getExistent(long id) throws DataHandlingException
+  {
+    return get(id).orElseThrow(()->
+            new DataHandlingException(String.format("Game with id %s is not in database", id)));
+  }
+}
---
+++ src/main/java/QuestionAnswerPairDatabaseManager.java
@@ -0,0 +1,66 @@
+package main.java;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import java.util.List;
+import java.util.Optional;
+
+public class QuestionAnswerPairDatabaseManager {
+  private static EntityManagerFactory emf = Persistence.createEntityManagerFactory("QuizUnit");
+  private EntityManager em = emf.createEntityManager();
+  private GameDatabaseManager gameDatabaseManager = new GameDatabaseManager();
+
+  public void save(QuestionAnswerPair pair)
+  {
+    var tx = em.getTransaction();
+    tx.begin();
+    em.persist(pair);
+    tx.commit();
+  }
+
+  public void update(QuestionAnswerPair pair)
+  {
+    var tx = em.getTransaction();
+    tx.begin();
+    em.merge(pair);
+    tx.commit();
+  }
+
+  public void remove(int id) throws DataHandlingException {
+    var existentGame = gameDatabaseManager.getExistent(id);
+    var tx = em.getTransaction();
+    tx.begin();
+
+    if (!em.contains(existentGame)) {
+      existentGame = em.merge(existentGame);
+    }
+    em.remove(existentGame);
+    tx.commit();
+  }
+
+  public Optional<QuestionAnswerPair> get(long id) throws DataHandlingException
+  {
+    return Optional.ofNullable(em.find(QuestionAnswerPair.class, id));
+  }
+
+  public List<QuestionAnswerPair> getPairs(long id) throws DataHandlingException
+  {
+    try {
+      var startIndex = gameDatabaseManager.get(id)
+              .map(Game::getCurrentPairId)
+              .orElse(1);
+      var query = em.createQuery("select p from QuestionAnswerPair p where p.id >= :startIndex");
+      query.setParameter("startIndex", startIndex);
+      return query.getResultList();
+    } catch (Exception e) {
+      throw new DataHandlingException(e);
+    }
+  }
+
+  public QuestionAnswerPair getExistent(long id) throws DataHandlingException
+  {
+    return get(id).orElseThrow(()->
+            new DataHandlingException(String.format("Pair with id %s is not in database", id)));
+  }
+}
