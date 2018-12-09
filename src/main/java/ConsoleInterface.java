d9dbc3b52a8e58259140cfb2e2520504ab88242c
chinwag <svetlanafedorishcheva99@gmail.com>
2018-12-09 21:24:59 +0500

    класс переделан для работы с несколькими менеджерами и user input handler

--- src/main/java/ConsoleInterface.java
+++ src/main/java/ConsoleInterface.java
@@ -1,23 +1,23 @@
 package main.java;
 
 import java.io.PrintStream;
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class ConsoleInterface {
   private static Scanner in = new Scanner(System.in);
   private static PrintStream out = new PrintStream(System.out);
   private static Game game;
-  private static QuizDatabaseManager databaseManager = new QuizDatabaseManager();
+  private static GameDatabaseManager gameDatabaseManager = new GameDatabaseManager();
+  private static UserInputHandler userInputHandler = new UserInputHandler();
 
   public static void main(String[] args) {
-    game = new Game((long)-1, databaseManager);
+    game = new Game((long)-1, new QuestionAnswerPairDatabaseManager(gameDatabaseManager));
     salute();
     play();
 
     try {
       game = null;
-      databaseManager.removeGame((long)-1);
+      gameDatabaseManager.remove((long)-1);
     } catch (DataHandlingException e) {/*game is already not in database*/}
   }
 
@@ -43,17 +43,12 @@ public class ConsoleInterface {
     }
     var currentInput = in.nextLine();
 
-    if (EditModeCommand.isUserInputStartingEditMode(currentInput, game) || game.isEditMode) {
-      out.println(handleEditModeCommand(game, currentInput));
-      return;
-    }
-
-    if (UserCommand.isUserInputCommand(currentInput)) {
-      handleUserCommand(currentInput);
+    if (userInputHandler.isUserInputCommand(currentInput, game)) {
+      out.println(userInputHandler.handle(game, currentInput));
     } else {
       var isAnswerCorrect = game.checkAnswer(currentInput);
       if (isAnswerCorrect) {
-        databaseManager.updateGame(game);
+        gameDatabaseManager.update(game);
       }
       out.println(isAnswerCorrect ? Answers.CORRECT.getMessage() : Answers.INCORRECT.getMessage());
       out.println();
@@ -63,26 +58,5 @@ public class ConsoleInterface {
   private static void salute() {
     out.println("Это математическая викторина. Я задаю вопрос - вы отвечаете.\n" +
             "За каждый правильный ответ получаете очки. Начинаем!");
-  }
-
-  private static void handleUserCommand(String userInput){
-    var userCommand = userInput.substring(1).toUpperCase();
-    if (UserCommand.isValidUserCommand(userCommand)){
-      var command = UserCommand.valueOf(userCommand);
-      out.println(command.execute(game));
-    } else {
-      out.println(String.format("Команды %s не существует", userCommand));
-    }
-  }
-
-  private static String handleEditModeCommand(Game game, String userInput) {
-    var arguments = userInput.split(" ");
-    var editModeCommand = arguments[0].substring(1).toUpperCase();
-    if (EditModeCommand.isValidEditModeCommand(editModeCommand, game)) {
-      var command = EditModeCommand.valueOf(editModeCommand);
-      return command.execute(game, String.join(" ", Arrays.copyOfRange(arguments, 1, arguments.length)));
-    } else {
-      return String.format("Команды %s не существует", arguments[0]);
-    }
   }
 }
