b307605ea1086450296b4e12e0cbb8e0e3014be8
chinwag <svetlanafedorishcheva99@gmail.com>
2018-12-16 18:24:03 +0500

    сюда перенесен парсинг ввода

--- src/main/java/UserInputHandler.java
+++ src/main/java/UserInputHandler.java
@@ -1,23 +1,22 @@
 package main.java;
 
-import java.util.Arrays;
 
-public class UserInputHandler {
+public class UserInputHandler implements InputHandler {
+  @Override
+  public boolean canHandle(Game game, String userMessage) {
+    if (isNotInputCommand(userMessage)) {
+      return false;
+    }
+
+    var arguments = userMessage.split(" ");
+    var userCommand = arguments[0].substring(1).toUpperCase();
+    return UserCommand.isValidUserCommand(userCommand);
+  }
+
   public String handle(Game currentGame, String userInput) {
     var arguments = userInput.split(" ");
     var userCommand = arguments[0].substring(1).toUpperCase();
-    if (EditModeCommand.isValidEditModeCommand(userCommand, currentGame)) {
-      var command = EditModeCommand.valueOf(userCommand);
-      return command.execute(currentGame, String.join(" ", Arrays.copyOfRange(arguments, 1, arguments.length)));
-    } else if (UserCommand.isValidUserCommand(userCommand)) {
-      var command = UserCommand.valueOf(userCommand);
-      return command.execute(currentGame);
-    } else {
-      return String.format("Команды %s не существует", arguments[0]);
-    }
-  }
-
-  public boolean isUserInputCommand(String input, Game game) {
-    return (input.length() > 0 && input.charAt(0) == '/') || game.isEditMode;
+    var command = UserCommand.valueOf(userCommand);
+    return command.execute(currentGame);
   }
 }
