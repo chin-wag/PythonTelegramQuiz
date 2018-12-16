b427b57b16ce10fed551f44b6cda86f2c9aa8474
ratbek <ratbeknurlanov@gmail.com>
2018-12-16 21:08:39 +0500

    Отрефакторить `GameManagerTest`

--- src/test/java/GameManagerTest.java
+++ src/test/java/GameManagerTest.java
@@ -1,93 +1,102 @@
 package test.java;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 import main.java.*;
 
 class GameManagerTest {
-  private static String unitName = "TestQuizUnit";
   private static GameDatabaseManager gameDatabaseManager;
-  private static QuestionAnswerPairDatabaseManager questionAnswerPairDatabaseManager;
   private static GameManager gameManager;
+  private final long gameId = 2;
 
   @BeforeEach
-  void initialize()
-  {
+  void initialize() {
+    var unitName = "TestQuizUnit";
     gameDatabaseManager = new GameDatabaseManager(unitName);
-    questionAnswerPairDatabaseManager = new QuestionAnswerPairDatabaseManager(unitName, gameDatabaseManager);
+    var questionAnswerPairDatabaseManager = new QuestionAnswerPairDatabaseManager(unitName, gameDatabaseManager);
     gameManager = new GameManager(gameDatabaseManager, questionAnswerPairDatabaseManager);
   }
 
+  @AfterEach
+  void afterEach() {
+    try {
+      if (gameDatabaseManager.isExistent(gameId))
+        gameDatabaseManager.remove(gameId);
+    } catch (DataHandlingException e) {
+      e.printStackTrace();
+    }
+  }
+
   @Test
-  void testAddNewGame() throws DataHandlingException {
-    gameManager.handleUserRequest((long)-2, " ");
-    var game = gameDatabaseManager.get((long)-2);
+  void testAddNewGame() {
+    gameManager.handleUserRequest(gameId, " ");
+
+    var game = gameDatabaseManager.get(gameId);
     assertTrue(game.isPresent());
-    gameDatabaseManager.remove((long)-2);
   }
 
   @Test
-  void testGetCurrentQuestion() throws DataHandlingException {
-    gameManager.handleUserRequest((long)-2, " ");
-    var game = gameDatabaseManager.get((long)-2);
+  void testGetCurrentQuestion() {
+    gameManager.handleUserRequest(gameId, " ");
+
+    var game = gameDatabaseManager.get(gameId);
     assertEquals("2+2", game.get().getCurrentQuestion());
-    gameDatabaseManager.remove((long)-2);
   }
 
   @Test
-  void testScoreIncrement() throws DataHandlingException {
-    gameManager.handleUserRequest((long)-2, " ");
-    var game = gameDatabaseManager.get((long)-2);
+  void testScoreIncrement() {
+    gameManager.handleUserRequest(gameId, " ");
+
+    var game = gameDatabaseManager.get(gameId);
     assertEquals(0, game.get().getScore());
 
-    gameManager.handleUserRequest((long)-2, "4");
-    game = gameDatabaseManager.get((long)-2);
+    gameManager.handleUserRequest(gameId, "4");
+
+    game = gameDatabaseManager.get(gameId);
     assertEquals(1, game.get().getScore());
-    gameDatabaseManager.remove((long)-2);
   }
 
   @Test
-  void testWrongAnswer() throws DataHandlingException {
-    gameManager.handleUserRequest((long)-2, " ");
+  void testWrongAnswer() {
+    gameManager.handleUserRequest(gameId, " ");
 
-    var expectedScore = gameDatabaseManager.get((long)-2).get().getScore();
-    var expectedPairId = gameDatabaseManager.get((long)-2).get().getCurrentPairId();
+    var expectedScore = gameDatabaseManager.get(gameId).get().getScore();
+    var expectedPairId = gameDatabaseManager.get(gameId).get().getCurrentPairId();
 
-    gameManager.handleUserRequest((long)-2, "0");
-    var game = gameDatabaseManager.get((long)-2);
+    gameManager.handleUserRequest(gameId, "0");
+
+    var game = gameDatabaseManager.get(gameId);
     assertEquals(expectedScore, game.get().getScore());
     assertEquals(expectedPairId, game.get().getCurrentPairId());
-    gameDatabaseManager.remove((long)-2);
   }
 
   @Test
-  void testNoScoreIncrementIfWrongAnswer() throws DataHandlingException {
-    gameManager.handleUserRequest((long)-2, " ");
-    gameManager.handleUserRequest((long)-2, "3");
-    var game = gameDatabaseManager.get((long)-2);
+  void testNoScoreIncrementIfWrongAnswer() {
+    gameManager.handleUserRequest(gameId, " ");
+    gameManager.handleUserRequest(gameId, "3");
+
+    var game = gameDatabaseManager.get(gameId);
     assertEquals(0, game.get().getScore());
-    gameDatabaseManager.remove((long)-2);
   }
 
   @Test
-  void testStopCommand() throws DataHandlingException {
-    gameManager.handleUserRequest((long)-2, " ");
-    gameManager.handleUserRequest((long)-2, "/stop");
-    assertFalse(gameDatabaseManager.isExistent((long)-2));
-    if (gameDatabaseManager.isExistent((long)-2)) {
-      gameDatabaseManager.remove((long)-2);
-    }
+  void testStopCommand() {
+    gameManager.handleUserRequest(gameId, " ");
+    gameManager.handleUserRequest(gameId, "/stop");
+
+    assertFalse(gameDatabaseManager.isExistent(gameId));
   }
 
   @Test
-  void testRemoveAfterEndOfQuestions() throws DataHandlingException {
-    gameManager.handleUserRequest((long)-2, " ");
-    gameManager.handleUserRequest((long)-2, "4");
-    gameManager.handleUserRequest((long)-2, "6");
-    assertFalse(gameDatabaseManager.isExistent((long)-2));
-    if (gameDatabaseManager.isExistent((long)-2)) {
-      gameDatabaseManager.remove((long)-2);
-    }
+  void testRemoveAfterEndOfQuestions() {
+    gameManager.handleUserRequest(gameId, " ");
+    gameManager.handleUserRequest(gameId, "4");
+    gameManager.handleUserRequest(gameId, "6");
+
+    assertFalse(gameDatabaseManager.isExistent(gameId));
   }
 }
